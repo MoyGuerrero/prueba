2caa083f75f70b637dfdd6c6e102611591fb89d8
MoyGuerrero <84990125+MoyGuerrero@users.noreply.github.com>
2023-01-18 20:23:14 -0600

    Resolucion del ejercicio 2

--- 2.js
+++ 2.js
@@ -2,7 +2,42 @@
 // Dada una matriz de N elementos repetidos,
 // crea una función numbersTop para obtener los tres elementos más repetidos ordenados de forma descendente por número de repeticiones.
 
+/**
+ * 
+ * @param {array} array Recibe un arreglo para obtener los primeros tres elementos repetidos
+ */
+
+
 function numbersTop(array) {
+    array.sort();
+
+    let aux;
+    let contador = 1;
+    let cantidad_repetido = [];
+    let norepetido = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i + 1] === array[i]) {
+            contador++;
+        } else {
+            cantidad_repetido.push(contador);
+            norepetido.push(array[i]);
+            contador = 1;
+        }
+    }
+
+    for (let i = 0; i < cantidad_repetido.length; i++) {
+        for (let j = i + 1; j < cantidad_repetido.length; j++) {
+            if (cantidad_repetido[i] < cantidad_repetido[j]) {
+                aux = norepetido[i];
+                norepetido[i] = norepetido[j];
+                norepetido[j] = aux;
+            }
+        }
+
+    }
+
+    console.log(norepetido.slice(0, 3));
 }
 
 /**
